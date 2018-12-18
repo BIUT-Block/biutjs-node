0e52534477a3870631e40ea4ea2e25354f0c5a9b
Zu Weihan <zuweihan1992@gmail.com>
2018-12-18 14:35:45 +0100

    updagte packages

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const SECCORE = require('@sec-block/secjs-core')
+const SECRPC = require('@sec-block/secjs-rpc')
 
 let core = new SECCORE()
+let rpc = new SECRPC(core)
 
 core.run()
+rpc.runRPCServer()
