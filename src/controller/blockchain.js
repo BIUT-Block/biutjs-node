ee097e0c53fb4169ac98c4893cba012072318c51
Zu Weihan <zuweihan1992@gmail.com>
2019-05-27 13:31:17 +0200

    verify balance is positive

--- src/controller/blockchain.js
+++ src/controller/blockchain.js
@@ -222,7 +222,7 @@ class BlockChain {
           callback(err, null)
         } else {
           let result = false
-          if (parseFloat(balance) >= parseFloat(tx.Value)) {
+          if ((parseFloat(balance) >= parseFloat(tx.Value)) && (parseFloat(balance) >= 0)) {
             this.senChain.getBalance(tx.TxFrom, (err, _balance) => {
               if (err) {
                 callback(err, null)
