5e59beaf9423748eb7e8ebfda211759cd398ea34
abysmli <abysmli@gmail.com>
2019-09-24 10:49:15 +0200

    dont use this version

--- src/controller/center-controller.js
+++ src/controller/center-controller.js
@@ -72,6 +72,9 @@ class CenterController {
     if (process.env.network && this.runningFlag === false) {
       this.initNetwork()
     }
+    setInterval(() => {
+      this._resetNetwork()
+    }, 10000)
   }
 
   _initNDP () {
@@ -176,6 +179,7 @@ class CenterController {
   }
 
   initNetwork () {
+    console.log('initNetwork')
     // -------------------------  IMPORTANT INSTANT  -------------------------
     this.runningFlag = true
     this.config.rlp = this.rlp
@@ -244,29 +248,81 @@ class CenterController {
   }
 
   _resetNetwork () {
+    // console.log('resetNetwork')
+    // this.config.dbconfig.logger.info(chalk.green(`RLP Peers 0, resetNetwork`))
+    // let peers = this.ndp.getPeers()
+    // this.config.dbconfig.logger.info(peers.toString())
+    // console.log(peers)
+    // peers.forEach(peer => {
+    //   this.ndp.removePeer(peer)
+    // })
+    // // add bootstrap nodes
+    // const BOOTNODES = this.bootstrapNodes.map(node => {
+    //   return {
+    //     address: node.ip,
+    //     udpPort: node.port,
+    //     tcpPort: node.port
+    //   }
+    // })
+    // for (let bootnode of BOOTNODES) {
+    //   this.ndp.bootstrap(bootnode).catch(err => {
+    //     this.config.dbconfig.logger.error(`${err.stack || err}`)
+    //     console.error(chalk.bold.red(err.stack || err))
+    //   })
+    // }
+    // this.config.dbconfig.logger.info(peers.toString())
+    // console.log('ResetNetwork Finish')
+    // console.log(this.ndp.getPeers())
+    // console.log(this.rlp.getPeers().length)
+    // -------------------------  NODE DISCOVERY PROTOCOL  -------------------------
     console.log('resetNetwork')
-    let peers = this.ndp.getPeers()
-    console.log(peers)
-    peers.forEach(peer => {
-      this.ndp.removePeer(peer)
+    this.ndp = {}
+    this.rlp = {}
+
+    this.ndp = new SECDEVP2P.NDP(this.config.PRIVATE_KEY, {
+      refreshInterval: SECConfig.SECBlock.devp2pConfig.ndp.refreshInterval,
+      timeout: SECConfig.SECBlock.devp2pConfig.ndp.timeout,
+      endpoint: SECConfig.SECBlock.devp2pConfig.ndp.endpoint
     })
-    // add bootstrap nodes
-    const BOOTNODES = this.bootstrapNodes.map(node => {
-      return {
-        address: node.ip,
-        udpPort: node.port,
-        tcpPort: node.port
-      }
+
+    // ------------------------  RLP TRANSPORT PROTOCOL  -----------------------
+    this.rlp = new SECDEVP2P.RLPx(this.config.PRIVATE_KEY, {
+      ndp: this.ndp,
+      timeout: SECConfig.SECBlock.devp2pConfig.rlp.timeout,
+      maxPeers: SECConfig.SECBlock.devp2pConfig.rlp.maxPeers,
+      capabilities: [
+        SECDEVP2P.SEC.sec
+      ],
+      clientId: SECConfig.SECBlock.devp2pConfig.rlp.clientId,
+      remoteClientIdFilter: SECConfig.SECBlock.devp2pConfig.rlp.remoteClientIdFilter,
+      listenPort: null
     })
-    for (let bootnode of BOOTNODES) {
-      this.ndp.bootstrap(bootnode).catch(err => {
-        this.config.dbconfig.logger.error(`${err.stack || err}`)
-        console.error(chalk.bold.red(err.stack || err))
-      })
+
+    this.nodesIPSync = new NodesIPSync()
+
+    // -------------------  NETWORK STATE MACHINE FLAG  -------------------
+    this.syncInfo = {
+      flag: false,
+      address: null,
+      timer: null
     }
-    console.log('resetNetwork Finish')
-    console.log(this.ndp.getPeers())
-    console.log(this.rlp.getPeers().length)
+    this.NetworkEventContainer = {}
+    let config = this.config
+    this.config.syncInfo = this.syncInfo
+    config.chainName = 'SEC'
+    config.chainID = '010001'
+    config.dbconfig.DBPath = config.dbconfig.SecDBPath
+    this.secChain = new BlockChain(config)
+
+    config.chainName = 'SEN'
+    config.chainID = '010002'
+    config.dbconfig.DBPath = config.dbconfig.SenDBPath
+    config.secChain = this.secChain
+    this.senChain = new BlockChain(config)
+    this.secChain.setSenChain(this.senChain)
+    if (process.env.network) {
+      this.initNetwork()
+    }
   }
 
   _refreshDHTConnections () {
